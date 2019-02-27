4b8b421d0193b1da6f17914e53daf92376ee0e4a
chris <christopherchateau@gmail.com>
2019-02-26 18:50:24 -0700

    two fer

---
+++ two-fer/two-fer.js
@@ -0,0 +1,11 @@
+class TwoFer {
+  // constructor(name) {
+  //   this.name = name;
+  // }
+
+  twoFer = (name) => {
+    return `One for ${name}, one for me.`;
+  };
+}
+
+module.exports = TwoFer;
